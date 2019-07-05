69a5cb65afd19a3d3129750139720a8cbd9e0df2
Alice Brunel <alc.brunel@gmail.com>
2019-07-05 18:13:10 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,12 @@
 # Build your say_hello method here
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 
 say_hello()
+=======
+def say_hello()
+  "Hello!"
+end
+>>>>>>> 923ee848cf8538533a803badf8702d035e0a0f20
